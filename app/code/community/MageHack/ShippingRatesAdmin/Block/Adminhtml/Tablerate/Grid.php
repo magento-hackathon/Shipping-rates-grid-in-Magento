a3b4ed3be1c7b2d2b1937a20f38252737c515017
git <john@blueclaw.co.uk>
2014-01-31 23:19:38 +0000

    amended how label for method column is retrieved

--- app/code/community/MageHack/ShippingRatesAdmin/Block/Adminhtml/Tablerate/Grid.php
+++ app/code/community/MageHack/ShippingRatesAdmin/Block/Adminhtml/Tablerate/Grid.php
@@ -4,4 +4,43 @@ class MageHack_ShippingRatesAdmin_Block_Adminhtml_Tablerate_Grid extends Mage_Ad
 {
 
     
+    /**
+     * Prepare table columns
+     *
+     * @return Mage_Adminhtml_Block_Widget_Grid
+     */
+    protected function _prepareColumns()
+    {
+        $this->addColumn('dest_country', array(
+            'header'    => Mage::helper('adminhtml')->__('Country'),
+            'index'     => 'dest_country',
+            'default'   => '*',
+        ));
+
+        $this->addColumn('dest_region', array(
+            'header'    => Mage::helper('adminhtml')->__('Region/State'),
+            'index'     => 'dest_region',
+            'default'   => '*',
+        ));
+
+        $this->addColumn('dest_zip', array(
+            'header'    => Mage::helper('adminhtml')->__('Zip/Postal Code'),
+            'index'     => 'dest_zip',
+            'default'   => '*',
+        ));
+
+        $label = Mage::getStoreConfig('carriers/tablerate/condition_name');
+        
+        $this->addColumn('condition_value', array(
+            'header'    => $label,
+            'index'     => 'condition_value',
+        ));
+
+        $this->addColumn('price', array(
+            'header'    => Mage::helper('adminhtml')->__('Shipping Price'),
+            'index'     => 'price',
+        ));
+
+        return Mage_Adminhtml_Block_Widget_Grid::_prepareColumns();
+    }
 }
