a07b78d3b25d790e8f8b0efb9a70cb176c1c54bc
David Slater <david.slater@interactiv4.com>
2014-02-03 15:08:28 +0100

    Add delete action

--- app/code/community/MageHack/ShippingRatesAdmin/Block/Adminhtml/Tablerate/Edit.php
+++ app/code/community/MageHack/ShippingRatesAdmin/Block/Adminhtml/Tablerate/Edit.php
@@ -1,41 +1,49 @@
 <?php
 
-class MageHack_ShippingRatesAdmin_Block_Adminhtml_Tablerate_Edit extends Mage_Adminhtml_Block_Widget_Form_Container
-{
+class MageHack_ShippingRatesAdmin_Block_Adminhtml_Tablerate_Edit extends Mage_Adminhtml_Block_Widget_Form_Container {
+
     /**
      * Initialize form
      * Add standard buttons
      * Add "Save and Apply" button
      * Add "Save and Continue" button
      */
-    public function __construct()
-    {
+    public function __construct() {
         $this->_objectId = 'id';
         $this->_controller = 'adminhtml_tablerate';
         $this->_blockGroup = 'shippingratesadmin';
         parent::__construct();
 
-        
+
         $this->_addButton('save_and_continue_edit', array(
-            'class'   => 'save',
-            'label'   => Mage::helper('shippingratesadmin')->__('Save and Continue Edit'),
+            'class' => 'save',
+            'label' => Mage::helper('shippingratesadmin')->__('Save and Continue Edit'),
             'onclick' => 'editForm.submit($(\'edit_form\').action + \'back/edit/\')',
-        ), 10);
+                ), 10);
+
+        $shippingRate = Mage::registry('shippingrate');
+        if ($shippingRate->getPk()) {
+            $this->_addButton('duplicate', array(
+                'label' => Mage::helper('shippingratesadmin')->__('Duplicate'),
+                'class' => 'save',
+                'onclick' => "i4Duplicate()",
+            ));
+        } else {
+            $this->_removeButton('delete');
+        }
     }
-    
-    /** 
+
+    /**
      * Get Header text
      *
      * @return string
      */
-    public function getHeaderText()
-    {   
+    public function getHeaderText() {
         if (Mage::registry('shippingrate') && Mage::registry('shippingrate')->getPk()) {
             return $this->__('Edit Shipping Rate');
-        }   
-        else {
+        } else {
             return $this->__('New Shipping Rate');
-        }   
-    } 
-    
+        }
+    }
+
 }
--- app/code/community/MageHack/ShippingRatesAdmin/controllers/Adminhtml/TablerateController.php
+++ app/code/community/MageHack/ShippingRatesAdmin/controllers/Adminhtml/TablerateController.php
@@ -1,13 +1,12 @@
 <?php
 
-class MageHack_ShippingRatesAdmin_Adminhtml_TablerateController extends Mage_Adminhtml_Controller_Action
-{
+class MageHack_ShippingRatesAdmin_Adminhtml_TablerateController extends Mage_Adminhtml_Controller_Action {
 
     protected function _initAction() {
         $this->loadLayout()->_setActiveMenu('system/shippingratesadmin');
         return $this;
-    }   
- 
+    }
+
     public function indexAction() {
         // If loading the page without a site specified, use the last site 
         // specified so the user doesn't have to keep selecting.
@@ -15,104 +14,101 @@ class MageHack_ShippingRatesAdmin_Adminhtml_TablerateController extends Mage_Adm
         if (is_null($siteId)) {
             $siteId = Mage::getSingleton('admin/session')->getShippingRatesAdminSiteId();
         }
-        $this->getRequest()->setParam('website', $siteId); 
+        $this->getRequest()->setParam('website', $siteId);
         Mage::getSingleton('admin/session')->setShippingRatesAdminSiteId($siteId);
-        
-        
+
+
         $this->_initAction()
-            ->renderLayout();
+                ->renderLayout();
     }
-    
+
     public function gridAction() {
         $this->getResponse()->setBody(
-            $this->getLayout()->createBlock('shippingratesadmin/adminhtml_tablerate_grid')->toHtml()
+                $this->getLayout()->createBlock('shippingratesadmin/adminhtml_tablerate_grid')->toHtml()
         );
     }
-    
+
     /**
      * Add New Rate action
      */
     public function newAction() {
-	$this->_forward('edit');
-		
+        $this->_forward('edit');
     }
-    
+
     public function editAction() {
-        $this->_initAction();
-        
-        $id  = $this->getRequest()->getParam('id');
+        $id = $this->getRequest()->getParam('id');
         $shippingrate = new Varien_Object(); //Mage::getModel('shipping/carrier_tablerate');
-        
+
         if ($id) {
             // Load record
-            
+
             $resource = Mage::getResourceModel('shipping/carrier_tablerate');
             $adapter = Mage::getSingleton('core/resource')->getConnection('core_read');
-            $binds = array( 'id' => $id);
+            $binds = array('id' => $id);
             $query = "select * FROM {$resource->getMainTable()} WHERE `pk` = :id";
             $data = $adapter->fetchRow($query, $binds);
-            
-            
-     
+
+
+
             // Check if record is loaded
             if (!$data) {
                 Mage::getSingleton('adminhtml/session')->addError($this->__('This Shipping Rate no longer exists.'));
                 $this->_redirect('*/*/');
-     
+
                 return;
             } else {
                 $shippingrate->setData($data);
-            }   
+            }
         } else {
             // for new rates set the default country to the country of the store
-            $shippingrate->setData('dest_country_id',Mage::getStoreConfig('general/country/default'));
-            
-            $shippingrate->setData('website_id',Mage::app()->getRequest()->getParam('website'));
+            $shippingrate->setData('dest_country_id', Mage::getStoreConfig('general/country/default'));
+
+            $shippingrate->setData('website_id', Mage::app()->getRequest()->getParam('website'));
         }
-        
+
         Mage::register('shippingrate', $shippingrate);
+
+        $this->_initAction();
         $this->renderLayout();
     }
-    
+
     public function regionAction() {
         $countrycode = $this->getRequest()->getParam('country');
         $state = "<option value=''>Please Select</option>";
         if ($countrycode != '') {
-            $statearray = Mage::getModel('directory/region')->getResourceCollection() ->addCountryFilter($countrycode)->load();
-            if($statearray->getSize() == 0)
-              $state = "<option value='0'>" . $this->__('No regions defined for this country.') . "</option>";  
-            
+            $statearray = Mage::getModel('directory/region')->getResourceCollection()->addCountryFilter($countrycode)->load();
+            if ($statearray->getSize() == 0)
+                $state = "<option value='0'>" . $this->__('No regions defined for this country.') . "</option>";
+
             foreach ($statearray as $_state) {
                 $state .= "<option value='" . $_state->getCode() . "'>" . $_state->getDefaultName() . "</option>";
             }
         }
         $this->getResponse()->setBody($state);
     }
-    
-    
-    public function saveAction()
-    {
-        if($this->getRequest()->getPost())  {
-           try {
-                $data = $this->getRequest()->getPost();
-         
-                $binds = array( 
-                                    'website_id' => $data['website_id'],
-                                    'dest_country_id' => $data['dest_country_id'],
-                                    'dest_region_id' => $data['dest_region_id'],
-                                    'dest_zip' => $data['dest_zip'],
-                                    'condition_value' => $data['condition_value'],
-                                    'price' => $data['price'],
-                                    'condition_name' => Mage::getStoreConfig('carriers/tablerate/condition_name')
-                                    );
-                
-                
-                $resource = Mage::getResourceModel('shipping/carrier_tablerate');
-                $adapter = Mage::getSingleton('core/resource')->getConnection('core_write');;
-                
 
-                if(isset($data['pk']))
-                {
+    public function saveAction() {
+        if ($this->getRequest()->getPost()) {
+            try {
+                $data = $this->getRequest()->getPost();
+
+                $binds = array(
+                    'website_id' => $data['website_id'],
+                    'dest_country_id' => $data['dest_country_id'],
+                    'dest_region_id' => $data['dest_region_id'],
+                    'dest_zip' => $data['dest_zip'],
+                    'condition_value' => $data['condition_value'],
+                    'price' => $data['price'],
+                    'condition_name' => Mage::getStoreConfig('carriers/tablerate/condition_name')
+                );
+
+
+                $resource = Mage::getResourceModel('shipping/carrier_tablerate');
+                $adapter = Mage::getSingleton('core/resource')->getConnection('core_write');
+                ;
+
+
+                if (isset($data['pk'])) {
                     $binds['pk'] = $data['pk'];
                     // Update existing
                     $query = "UPDATE {$resource->getMainTable()} "
@@ -124,20 +120,19 @@ class MageHack_ShippingRatesAdmin_Adminhtml_TablerateController extends Mage_Adm
                             . "`condition_name` = :condition_name, "
                             . "`price` = :price "
                             . "WHERE `pk` = :pk ";
-                    
-    
-                    $adapter->query($query, $binds);
 
-                }else{
-                   
+
+                    $adapter->query($query, $binds);
+                } else {
+
                     // Add New
                     $query = "insert into {$resource->getMainTable()} (website_id, dest_country_id, dest_region_id, dest_zip, condition_value, condition_name, price) "
-                    . "values (:website_id, :dest_country_id, :dest_region_id, :dest_zip, :condition_value, :condition_name, :price)";                   
-                    
-                    
+                            . "values (:website_id, :dest_country_id, :dest_region_id, :dest_zip, :condition_value, :condition_name, :price)";
+
+
                     $adapter->query($query, $binds);
                 }
- 
+
                 Mage::getSingleton('adminhtml/session')->addSuccess($this->__('The Shipping Rate has been saved.'));
                 // The following line decides if it is a "save" or "save and continue"
                 if ($this->getRequest()->getParam('back')) {
@@ -145,40 +140,66 @@ class MageHack_ShippingRatesAdmin_Adminhtml_TablerateController extends Mage_Adm
                 } else {
                     $this->_redirect('*/*/', array('website' => $data['website_id']));
                 }
- 
+
                 return;
-            }   
-            catch (Mage_Core_Exception $e) {
+            } catch (Mage_Core_Exception $e) {
                 Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+            } catch (Exception $e) {
+                Mage::getSingleton('adminhtml/session')->addError($this->__('An error occurred while saving this Shipping Rate.' . $e->getMessage()));
             }
-            catch (Exception $e) {
-                Mage::getSingleton('adminhtml/session')->addError($this->__('An error occurred while saving this Shipping Rate.'. $e->getMessage()));
-            } 
-            
+
             $this->_redirectReferer();
         }
     }
-    
+
     /**
      * Fired when you change the website selected in the editing form to return
      * website specific configuration that may affect fields in the form such as 
      * the condition (weight/destination, price/destination...)
      */
     public function changewebsiteAction() {
-      
+
         $website = $this->getRequest()->getParam('website');
-        
+
         // Update the current website for when you return to the grid.
         Mage::getSingleton('admin/session')->setShippingRatesAdminSiteId($website);
-        
+
         $conditionLabel = $label = Mage::getSingleton('shipping/carrier_tablerate')
-            ->getCode('condition_name_short', $this->_getShippingRatesAdminHelper()->getWebsiteConfigData('carriers/tablerate/condition_name', $website))
-            . " <span class=\"required\">*</span>";
+                        ->getCode('condition_name_short', $this->_getShippingRatesAdminHelper()->getWebsiteConfigData('carriers/tablerate/condition_name', $website))
+                . " <span class=\"required\">*</span>";
         $data = array('conditionLabel' => $conditionLabel);
         $this->getResponse()->setHeader('Content-type', 'application/json');
-        $this->getResponse()->setBody(json_encode($data));        
+        $this->getResponse()->setBody(json_encode($data));
+    }
+
+    public function deleteAction() {
+        $id = $this->getRequest()->getParam('id');
+        $success = false;
+        try {
+            if (!$id) {
+                Mage::throwException($this->__("Please specify a rate to delete."));
+            }
+            
+            $resource = Mage::getResourceModel('shipping/carrier_tablerate'); /* @var $resource Mage_Shipping_Model_Resource_Carrier_Tablerate */
+            $adapter = Mage::getSingleton('core/resource')->getConnection('core_write'); /* @var $adapter Zend_Db_Adapter_Abstract */
+            $numRowsAffected = $adapter->delete($resource->getMainTable(), $adapter->quoteInto(" `pk` = ? ", $id));
+            if ($numRowsAffected == 0) {
+                Mage::throwException($this->__("The rate could not be deleted because it no longer exists. It was probably already deleted."));
+            }
+            $success = true;
+        } catch (Mage_Core_Exception $ex ) {
+            $this->_getSession()->addError($ex->getMessage());
+        } catch (Exception $ex) {
+            $this->_getSession()->addError($this->__("An problem occurred attempting to delete the tablerate: '%s'.", $ex->getMessage()));
+        }
+        if ($success) {
+            $this->_getSession()->addSuccess($this->__("The rate was successfully deleted."));
+        }
+        
+        $this->_redirect("*/*/index");
     }
     
+
     /**
      * 
      * @return MageHack_ShippingRatesAdmin_Helper_Data
@@ -186,5 +207,5 @@ class MageHack_ShippingRatesAdmin_Adminhtml_TablerateController extends Mage_Adm
     protected function _getShippingRatesAdminHelper() {
         return Mage::helper('shippingratesadmin');
     }
-    
+
 }
